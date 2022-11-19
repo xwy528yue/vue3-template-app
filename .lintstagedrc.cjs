f699ad8782fceff03d9051541d11bcf7bc64eb34
KERO\xwy52 <xwy258yue@163.com>
2022-11-19 23:06:38 +0800

    build: add stylelint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
-  '*': 'prettier --write -u',
-  '*.{js,ts,vue,tsx}': ['eslint --fix', 'prettier --write'],
+  '**/*': 'prettier --write -u',
+  '**/*.{css,scss,sass,less,vue}': 'stylelint',
+  '**/*.{js,ts,vue,tsx}': ['eslint --fix', 'prettier --write'],
 }
